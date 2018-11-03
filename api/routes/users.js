f09decd3e3a9e43d261dc41b67ff23c6d3266a09
Manav Bokinala <manav.bokinala@gmail.com>
2018-11-03 12:09:10 -0700

    Initial Commit

---
+++ api/routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+
+router.post('/', (req, res) => {
+    console.log('Got Request!');
+});
---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+
+const app = express();
+
+const userRoutes = require('./api/routes/users');
+
+mongoose.connect(`mongodb://api:n3ighbor@ds151293.mlab.com:51293/smart-rides`, {
+	useNewUrlParser: true
+});
+
+app.disable('etag');
+
+app.use(morgan('dev'));
+
+app.get('/*', function(req, res, next){ 
+	res.setHeader('Last-Modified', (new Date()).toUTCString());
+	next(); 
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	if(req.method === 'OPTIONS'){
+		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+		res.status(200).send('');
+	}
+	next();
+});
+
+app.use('/users', userRoutes);
+
+app.use((req, res, next) => {
+	const error = new Error('Not found');
+	error.status = 400;
+	next(error);
+});
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	console.log('Error: ' + error.message);
+	res.json({
+		message: error.message
+	});
+});
+
+module.exports = app;
