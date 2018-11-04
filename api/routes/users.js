1f1931bacfd1ff5cb020728dcd911caf97a2c94d
Manav Bokinala <manav.bokinala@gmail.com>
2018-11-03 17:42:50 -0700

    Bug fixes

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const mongoose = require('mongoose');
 
+const User = require('../models/user');
+
 router.post('/', (req, res) => {
     const user = new User({
         _id: new mongoose.Types.ObjectId(),
